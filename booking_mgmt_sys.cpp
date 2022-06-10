4bae7945fa1d6ab54cebbee4612ce95bd6b428c6
ApplePot <98685980+ApplePot@users.noreply.github.com>
2022-06-10 10:55:29 +0800

    Updated function output and added input restrictions

--- booking_mgmt_sys.cpp
+++ booking_mgmt_sys.cpp
@@ -4,7 +4,6 @@
 #include <iomanip>
 #include <list>
 #include <algorithm>
-
 using namespace std;
 
 struct Booking {
@@ -12,13 +11,15 @@ struct Booking {
 };
 
 bool operator<(const Booking &b1, const Booking &b2) {
+ 
     return (b1.name < b2.name);
+    
 }
  
-list<Booking> schedule;
+list<Booking>schedule;
 
 //copy data from input.txt into list
-void file_to_list(list<Booking> &schedule) {
+void file_to_list(list<Booking>& schedule) {
 
     ifstream ifile("input.txt");
 
@@ -92,7 +93,7 @@ void list_to_file(list<Booking> schedule) {
 }
 
 //list out all bookings on a given day
-void ListBooking(list<Booking> schedule) {
+void ListBooking(list<Booking>schedule) {
 
     string d;
 retry:
@@ -115,6 +116,11 @@ retry:
                 counter++;
             }
         }
+
+        if (counter == 0) {
+            cout << "There are no reservation on " << d << endl;
+            return;
+        }
         schedule.sort();
         //list out all bookings on d
         iter = schedule.begin();
@@ -135,7 +141,7 @@ retry:
 }
 
 //prints the total number of bookings in a week
-void TotalBooking(list<Booking> schedule) {
+void TotalBooking(list<Booking>schedule) {
     
     int count = schedule.size();
     if (count == 0) {
@@ -147,22 +153,39 @@ void TotalBooking(list<Booking> schedule) {
 
 }
 
-// Make a new booking to the schedule
-void AddBooking(list<Booking> &schedule) {
+//adds a booking to the schedule
+void Add_Booking(list<Booking>& schedule) {
 
-    string day, s_time, e_time, n, ppl, tableNo;
+    string dy, s_time, e_time, n, ppl, tableNo;
     bool clash = false;
-    cout << "Day of the week:" << endl;
-    cin >> day;
+    retry:
+    cout << "Day of the week: (h for help)" << endl;
+    cin >> dy;
+    if (dy == "h") {
+        cout << " Mon - Monday\n Tue - Tuesday\n Wed - Wednesday\n Thu - Thursday\n Fri - Friday\n Sat - Saturday\n Sun - Sunday\n";
+        system("pause");
+        system("cls");
+        goto retry;
+    }
+    else if (dy != "Mon" && dy != "Tue" && dy != "Wed" && dy != "Thu" && dy != "Fri" && dy != "Sat" && dy != "Sun") {
+        cout << "Invalid day input" << endl;
+        return;
+    }
+
     cout << "Start time: " << endl;
     cin >> s_time;
     if (s_time >= "2100") {
         cout << "Start time passed restaurant business hours" << endl;
         return;
     }
+    retype:
     cout << "End time: " << endl;
     cin >> e_time;
-    if (e_time >= "2100") {
+    if (e_time <= s_time) {
+        cout << "Invalid reservation end time, please re-enter the ending time" << endl;
+        goto retype;
+    }
+    else if (e_time >= "2100") {
         cout << "End time passed restaurant business hours" << endl;
         return;
     }
@@ -174,6 +197,10 @@ void AddBooking(list<Booking> &schedule) {
         cout << "Maximum occupancy for each table is 4." << endl;
         return;
     }
+    else if (stoi(ppl) <= 0) {
+        cout << "The minimum occupancy is 1" << endl;
+        return;
+    }
     cout << "Table Number: " << endl;
     cin >> tableNo;
     if (tableNo <= "00" || tableNo >= "25") {
@@ -183,7 +210,7 @@ void AddBooking(list<Booking> &schedule) {
 
     for (list<Booking>::iterator iter = schedule.begin(); (iter != schedule.end() && clash != true); iter++) {
 
-        if (day == iter->day) {
+        if (dy == iter->day) {
             if ((s_time >= iter->start_time && s_time <= iter->start_time)||(e_time >= iter->start_time && e_time <= iter->start_time)) {
                 if (tableNo == iter->table) {
                     clash = true;
@@ -197,7 +224,7 @@ void AddBooking(list<Booking> &schedule) {
         return;
     }
     else {
-        Booking b1 = { day, s_time, e_time, n, ppl, tableNo };
+        Booking b1 = { dy, s_time, e_time, n, ppl, tableNo };
         schedule.push_back(b1);
         cout << "Booking added to schedule" << endl;
     }
@@ -206,20 +233,48 @@ void AddBooking(list<Booking> &schedule) {
 }
 
 //remove a booking from the schedule
-void Rm_Booking(list<Booking> &schedule) {
+void Rm_Booking(list<Booking>& schedule) {
 
     string dy, s_time, e_time, n, tableNo;
     bool exist = false;
-    cout << "Day of reservation: " << endl;
+    retry:
+    cout << "Day of reservation: (h for help)" << endl;
     cin >> dy;
+    if (dy == "h") {
+        cout << " Mon - Monday\n Tue - Tuesday\n Wed - Wednesday\n Thu - Thursday\n Fri - Friday\n Sat - Saturday\n Sun - Sunday\n";
+        system("pause");
+        system("cls");
+        goto retry;
+    }
+    else if (dy != "Mon" && dy != "Tue" && dy != "Wed" && dy != "Thu" && dy != "Fri" && dy != "Sat" && dy != "Sun") {
+        cout << "Invalid day input" << endl;
+        return;
+    }
     cout << "Start time of reservation: " << endl;
     cin >> s_time;
+    if (s_time >= "2100") {
+        cout << "Start time passed restaurant business hours" << endl;
+        return;
+    }
+    retype:
     cout << "End time of reservation: " << endl;
     cin >> e_time;
+    if (e_time <= s_time) {
+        cout << "Invalid reservation end time, please re-enter the ending time" << endl;
+        goto retype;
+    }
+    else if (e_time >= "2100") {
+        cout << "End time passed restaurant business hours" << endl;
+        return;
+    }
     cout << "Name of reservation: " << endl;
     cin >> n;
     cout << "Table number reserved: " << endl;
     cin >> tableNo;
+    if (tableNo <= "00" || tableNo >= "25") {
+        cout << "Table Number out of range" << endl;
+        return;
+    }
 
     list<Booking>::iterator iter = schedule.begin();
     for (; iter != schedule.end(); iter++) {
@@ -244,35 +299,7 @@ void Rm_Booking(list<Booking> &schedule) {
 
 }
 
-//check avaliability of table
-//returns time period in which the table is occupied
-void CheckTime(list<Booking> schedule) {
-    string dy, tableNo;
-    bool exist = false;
-    cout << "Day of reservation: " << endl;
-    cin >> dy;
-    cout << "Table number reserved: " << endl;
-    cin >> tableNo;
-
-    list<Booking>::iterator iter = schedule.begin();
-    for (; iter != schedule.end(); iter++) {
-        if (iter->day == dy) {
-            if (iter->table == tableNo) {
-                exist = true;
-                cout << "------------------------------------------\n";
-                cout << iter->start_time << "~" << iter->end_time << endl;
-            }
-        }
-    }
-
-    if (!exist) {
-        cout << "Table " << tableNo << " does not have any reservations on record on " << dy << endl;
-        return;
-    }
-    cout << "------------------------------------------\n";
-}
-
-// print help menu for main function
+//print help menu for main function
 void h_cmd() {
 
     system("cls");
@@ -281,21 +308,76 @@ void h_cmd() {
     cout << "==========================================================\n";
     cout << setw(13) << left << "save" << setw(40) << left << "| Save current schedule to file" << endl;
     cout << "----------------------------------------------------------\n";
-    cout << setw(13) << left << "list" << setw(40) << left << "| List all current schedule booking" << endl;
+    cout << setw(13) << left << "listBooking" << setw(40) << left << "| List all current schedule booking" << endl;
     cout << "----------------------------------------------------------\n";
-    cout << setw(13) << left << "total" << setw(40) << left << "| View all booking for a day" << endl;
+    cout << setw(13) << left << "totBooking" << setw(40) << left << "| View all booking for a day" << endl;
     cout << "----------------------------------------------------------\n";
-    cout << setw(13) << left << "add" << setw(40) << left << "| Add a booking" << endl;
+    cout << setw(13) << left << "addBooking" << setw(40) << left << "| Add a booking" << endl;
     cout << "----------------------------------------------------------\n";
-    cout << setw(13) << left << "remove" << setw(40) << left << "| Remove a booking" << endl;
+    cout << setw(13) << left << "rmBooking" << setw(40) << left << "| Remove a booking" << endl;
     cout << "----------------------------------------------------------\n";
-    cout << setw(13) << left << "check" << setw(40) << left << "| Check the avaliability of a table" << endl;
+    cout << setw(13) << left << "chkTime" << setw(40) << left << "| Check the avaliability of a table" << endl;
     cout << "----------------------------------------------------------\n";
     cout << setw(13) << left << "exit" << setw(40) << left << "| Exit program" << endl;
     cout << "----------------------------------------------------------\n";
     system("pause");
     system("cls");
 
+}
+
+//check avaliability of table
+//returns time period in which the table is occupied
+void CheckTime(list<Booking>schedule) {
+
+    string dy, tableNo;
+    bool exist = false;
+    retry:
+    cout << "Day of reservation: (h for help)" << endl;
+    cin >> dy;
+    if (dy == "h") {
+        cout << " Mon - Monday\n Tue - Tuesday\n Wed - Wednesday\n Thu - Thursday\n Fri - Friday\n Sat - Saturday\n Sun - Sunday\n";
+        system("pause");
+        system("cls");
+        goto retry;
+    }
+    else if (dy != "Mon" && dy != "Tue" && dy != "Wed" && dy != "Thu" && dy != "Fri" && dy != "Sat" && dy != "Sun") {
+        cout << "Invalid day input" << endl;
+        return;
+    }
+    cout << "Table number reserved: " << endl;
+    cin >> tableNo;
+    if (tableNo <= "00" || tableNo >= "25") {
+        cout << "Table Number out of range" << endl;
+        return;
+    }
+
+    list<Booking>::iterator iter = schedule.begin();
+    for (; iter != schedule.end(); iter++) {
+        if (iter->day == dy) {
+            if (iter->table == tableNo) {
+                exist = true;
+            }
+        }
+    }
+
+    if (exist) {
+        cout << "=========================================\n";
+        cout << "== From/To == | == Name == | == People ==" << endl;
+        cout << "=========================================\n";
+        list<Booking>::iterator iter = schedule.begin();
+        for (; iter != schedule.end(); iter++) {
+            if (iter->day == dy) {
+                if (iter->table == tableNo) {
+                    cout << setw(8) << left << (*iter).start_time << "~" << (*iter).end_time << " | " << setw(10) << left << (*iter).name << " | " << setw(12) << left << (*iter).people << endl;
+                    cout << "-----------------------------------------\n";
+                }
+            }
+        }
+    }
+    else {
+        cout << "Table " << tableNo << " does not have any reservations on record on " << dy << endl;
+        return;
+    }
 }
 
 int main() {
@@ -313,19 +395,19 @@ int main() {
         else if (cmd == "save") {
             list_to_file(schedule);
         }
-        else if (cmd == "list") {
+        else if (cmd == "listBooking") {
             ListBooking(schedule);
         }
-        else if (cmd == "total") {
+        else if (cmd == "totBooking") {
             TotalBooking(schedule);
         }
-        else if (cmd == "add") {
-            AddBooking(schedule);
+        else if (cmd == "addBooking") {
+            Add_Booking(schedule);
         }
-        else if (cmd == "remove") {
+        else if (cmd == "rmBooking") {
             Rm_Booking(schedule);
         }
-        else if (cmd == "check") {
+        else if (cmd == "chkTime") {
             CheckTime(schedule);
         }
         else if (cmd == "exit") {
@@ -333,11 +415,11 @@ int main() {
             break;
         }
         else {
-            cout << "Invalid Command" << endl;
+            cout << "Invalid command" << endl;
         }
     }
 
-    cout << "Program Exited" << endl;
+    cout << "Program exited" << endl;
 	
 	return 0;
 }
